e7b31971ebe509bec36ec68fd4d361b8b66d9cba
bryandms <bryandms@hotmail.com>
2018-11-18 15:02:26 -0600

    Add private channels :closed_lock_with_key:

--- resources/js/components/ColumbogramComponent.vue
+++ resources/js/components/ColumbogramComponent.vue
@@ -45,7 +45,7 @@
         mounted() {
             this.getConversations()
 
-            Echo.channel(`users.${this.userId}`)
+            Echo.private(`users.${this.userId}`)
                 .listen('MessageSent', (data) => {
                     const message = data.message
                     message.written_by_me = false
